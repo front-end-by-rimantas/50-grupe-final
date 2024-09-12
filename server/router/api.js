c16cdb382a2aa78693655c75e70c894bd04a5ebc
Rimantas <belauzas@gmail.com>
2024-09-12 14:48:31 +0300

    Up

--- server/router/api.js
+++ server/router/api.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import { locationsAPIrouter } from './locations/locations.js';
 
 export const apiRouter = express.Router();
+
+apiRouter.use('/locations', locationsAPIrouter);
 
 apiRouter.all('/', (req, res) => {
     return res.json({
