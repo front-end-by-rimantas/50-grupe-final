ceebecc9063ff4d30106a4552b3d00b40a9dd11a
Rimantas <belauzas@gmail.com>
2024-09-19 13:28:31 +0300

    Up

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 
+import { useContext } from "react";
 import { Footer } from "../components/footer/Footer";
 import { Header } from "../components/header/Header";
+import { GlobalContext } from "../context/GlobalContext";
 
 export function Dashboard() {
-    const isLoggedIn = false;
+    const { isLoggedIn } = useContext(GlobalContext);
 
     return (
         <>
